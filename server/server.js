4e1292980a263d58ec469fa8e404a63dd9c45c95
jimjea <jimjea@gmail.com>
2014-08-24 20:58:06 -0700

    added map and geolocation api

---
+++ server/server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var req = require('/request-handler.js');
+
+var port = 3000;
+
+var ip = '127.0.0.1';
+
+var server = http.createServer(req.handleRequest);
+console.log("Listening on http://" + ip + : + port);
+server.listen(port, ip);
